7b26de556c76448ed20e1d11713fb2956900ac53
ClockworkConundrum <danny.livings@gmail.com>
2014-03-25 18:35:46 +0000

    Edited menus

---
+++ js/dropDownMenus.js
@@ -0,0 +1,32 @@
+{
+	var timeOut = 500;
+	var closeTimer = 0;
+	var currentMenuItem = 0;
+
+	function menuOpen(id) {
+
+		menuClose();
+
+		currentMenuItem = document.getElementById(id);
+		currentMenuItem.style.visibility = 'visible';
+	}
+
+	function menuClose() {
+		if (currentMenuItem) {
+			currentMenuItem.style.visibility = 'hidden';
+		}
+	}
+
+	function menuCloseOnTimer() {
+		closeTimer = window.setTimeout(menuClose, timeOut);
+	}
+
+	function menuCancelClose() {
+		if (closeTimer) {
+			window.clearTimeout(closeTimer);
+			closeTimer = null;
+		}
+	}
+
+	document.onClick = menuClose;
+}
